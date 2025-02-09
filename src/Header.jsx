28a032be29b86e9e13e9407ef07adcf477ebf908
Helga <helga128@gmail.com>
2025-02-09 20:45:12 +0000

    nav icons skipta um lit við val

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     return(
         <header className="">
             <div className="flex flex-col gap-2 mt-4 p-2 text-center sm:text-left">
-                <h1 className="font-bold text-6xl sm:text-7xl text-lime-500">spotsie</h1>
+                <a href="/"><h1 className="font-bold text-6xl sm:text-7xl text-lime-500">spotsie</h1></a>
                 <p className="text-xs sm:text-sm">uppáhalds lögin mín sem ég mæli með!</p>
             </div>
         </header>
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,11 +6,11 @@ function Nav() {
     return(
         <div className="grid grid-cols-4 gap-5 items-end">
             {[
-                { to: "/", img: "musical-note-icon.png", label: "Lög" },
-                { to: "/artists", img: "microphone-icon.png", label: "Artistar" },
-                { to: "/genres", img: "guitar-icon.png", label: "Stefnur" },
-                { to: "/random", img: "random-icon.png", label: "Random" },
-            ].map(({ to, img, label }) => (
+                { to: "/", img: "musical-note-icon.png", greenImg: "musical-note-green.png", label: "Lög" },
+                { to: "/artists", img: "microphone-icon.png", greenImg: "microphone-green.png", label: "Artistar" },
+                { to: "/genres", img: "guitar-icon.png", greenImg: "guitar-green.png", label: "Stefnur" },
+                { to: "/random", img: "random-icon.png", greenImg: "random-green.png", label: "Random" },
+            ].map(({ to, img, greenImg, label }) => (
                 <NavLink
                     key={to}
                     to={to}
@@ -20,7 +20,7 @@ function Nav() {
                 >
                     {({ isActive }) => (
                         <>
-                            <img src={`./assets/${img}`} alt={label} className="min-w-4 w-4 sm:w-6 lg:w-7" />
+                            <img src={`./assets/${isActive ? greenImg : img}`} alt={label} className="min-w-4 w-4 sm:w-6 lg:w-7" />
                             <p className="text-xs md:text-sm">{label}</p>
                             <div className={`border-b-2 w-4/5 transition-colors 
                                 ${isActive ? 'border-lime-500' : 'border-zinc-300 group-hover:border-lime-500'}`}></div>
