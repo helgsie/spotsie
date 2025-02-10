c6f790f73f5ce901574f77cd50cfd3f17df4129a
Helga <helga128@gmail.com>
2025-02-10 02:22:15 +0000

    breyta texta í header

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
         <header className="">
             <div className="flex flex-col gap-2 mt-4 p-2 text-center sm:text-left">
                 <a href="/"><h1 className="font-bold text-6xl sm:text-7xl text-lime-500">spotsie</h1></a>
-                <p className="text-xs sm:text-sm">uppáhalds lögin mín sem ég mæli með!</p>
+                <p className="text-xs sm:text-sm">hvað ég er að hlusta á í augnablikinu</p>
             </div>
         </header>
     );
