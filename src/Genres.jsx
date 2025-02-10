79841a614bc8865d04c4f77d86cfbbf048718398
Helga <helga128@gmail.com>
2025-02-10 02:07:23 +0000

    færði playlists á genre síðu

--- src/Genres.jsx
+++ src/Genres.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import Playlist from './Playlist.jsx'
+import Playlists from './Playlists.jsx'
 import Header from "./Header.jsx";
 import Nav from "./Nav.jsx";
 import Footer from './Footer.jsx';
@@ -12,27 +12,31 @@ function Genres() {
                 <Header/>
                 <Nav/>
             </div>
-            <div className="flex flex-col text-s gap-2">
-                <h2 className="font-bold text-s text-zinc-500">Stefnur</h2>
-                <div className="flex flex-col rounded-lg mb-3">
-                    <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
-                        <img src="./assets/rock-hand-icon.png" alt="" className="w-6"></img>
-                        <p className="">Nútímarokk</p>
-                    </a>
-                    <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
-                        <img src="./assets/microphone-icon.png" alt="" className="w-6"></img>
-                        <p>Popp</p>
-                    </a>
-                    <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
-                        <img src="./assets/korea-icon.png" alt="" className="w-6"></img>
-                        <p>Kpop</p>
-                    </a>
-                    <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
-                        <img src="./assets/tape-icon.png" alt="" className="w-6"></img>
-                        <p>Indie</p>
-                    </a>
+            <div className="mainGenrePage flex flex-col gap-3">
+                <div className="flex flex-col text-s gap-2">
+                    <h2 className="font-bold text-s text-zinc-500">Stefnur</h2>
+                    <div className="flex flex-col rounded-lg mb-3">
+                        <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
+                            <img src="./assets/rock-hand-icon.png" alt="" className="w-6"></img>
+                            <p className="">Nútímarokk</p>
+                        </a>
+                        <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
+                            <img src="./assets/microphone-icon.png" alt="" className="w-6"></img>
+                            <p>Popp</p>
+                        </a>
+                        <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
+                            <img src="./assets/korea-icon.png" alt="" className="w-6"></img>
+                            <p>Kpop</p>
+                        </a>
+                        <a href="#" className="flex transition ease-in-out hover:text-lime-500 p-2 rounded-lg gap-3 items-center">
+                            <img src="./assets/tape-icon.png" alt="" className="w-6"></img>
+                            <p>Indie</p>
+                        </a>
+                    </div>
                 </div>
+                <Playlists/>
             </div>
+            
             <Footer/>
         </div>
     );
